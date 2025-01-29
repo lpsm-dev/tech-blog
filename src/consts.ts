449d00659265775d391f76183d8796d343714613
lpsm-dev-github <lpsm-dev@protonmail.com>
2025-01-29 04:10:34 -0300

    feat: linkedin footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const GITHUB_REPO_URL = "https://github.com/lpsm-dev/tech-blog";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/lpsm-dev/";
 export const SITE_TITLE = "Tech Blog";
 export const SITE_DESCRIPTION = "My personal blog about tech 🧘";
