b84c3adf0e16cbcb6a774a0ba256c97720e2fef1
lpsm-dev-github <lpsm-dev@protonmail.com>
2025-01-29 05:27:58 -0300

    feat: add components

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,7 +14,7 @@ const dateSchema = z.preprocess((val) => {
 }, z.date().nullable().default(new Date()));
 
 const blog = defineCollection({
-  loader: glob({ base: "content", pattern: "**/*.{md,mdx}" }),
+  loader: glob({ base: "content/pt-br", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
     title: z.string(),
     subtitle: z.string().optional().nullable(),
