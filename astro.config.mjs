04d8f0e25c9b0b8917575f1e321c9cee97da0c83
lpsm-dev-github <lpsm-dev@protonmail.com>
2025-01-31 06:10:45 -0300

    feat: add search and others

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,29 +2,21 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import pagefind from "astro-pagefind";
+import icon from "astro-icon";
 import expressiveCode from "astro-expressive-code";
-import remarkMermaid from "remark-mermaid";
+import { mermaid } from "./src/plugins/astro-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blogger.lpsm.cloud",
   base: "/",
   markdown: {
-    remarkPlugins: [
-      [
-        remarkMermaid,
-        {
-          simple: true,
-          wrap: null,
-          mermaidConfig: {
-            theme: "default",
-            securityLevel: "loose",
-            startOnLoad: true,
-          },
-        },
-      ],
-    ],
+    remarkPlugins: [mermaid],
   },
-  integrations: [expressiveCode(), mdx(), sitemap(), tailwind()],
+  integrations: [expressiveCode(), mdx(), sitemap(), icon(), tailwind(), pagefind()],
   output: "static",
+  prefetch: {
+    defaultStrategy: 'tap'
+  }
 });
