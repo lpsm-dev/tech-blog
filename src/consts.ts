945861ea3499e6a0aa84a0aef245a7179e25a027
lpsm-dev-github <lpsm-dev@protonmail.com>
2025-02-01 18:34:23 -0300

    feat: add news about the service

--- src/consts.ts
+++ src/consts.ts
@@ -2,12 +2,29 @@ export const GITHUB_REPO_URL = "https://github.com/lpsm-dev/tech-blog";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/lpsm-dev/";
 export const SITE_TITLE = "Tech Blog";
 export const SITE_DESCRIPTION = "My personal blog about tech 🧘";
-export const PAGE_TAGS_TITLE = "Explore as tags";
-export const PAGE_TAGS_DESCRIPTION =
-  "Navegue pelas tags e busque os artigos relacionados a cada tag";
-export const PAGE_PROJECTS_TITLE = "Explore meus projetos";
-export const PAGE_PROJECTS_DESCRIPTION =
-  "Aqui temos os projetos pessoais que desenvolvi ao longo do tempo.";
+export const EMAIL = "cloud-craft-solutions@proton.me";
+
+/*
+ * About - Page titles and descriptions
+ */
 export const PAGE_ABOUT_TITLE = "Hey! Eu sou o Lucca";
 export const PAGE_ABOUT_DESCRIPTION =
   "Conheça um pouco mais sobre mim e o que faço";
+/*
+ * Projects - Page titles and descriptions
+ */
+export const PAGE_PROJECTS_TITLE = "Explore meus projetos";
+export const PAGE_PROJECTS_DESCRIPTION =
+  "Aqui temos os projetos pessoais que desenvolvi ao longo do tempo.";
+/*
+ * Services - Page titles and descriptions
+ */
+export const PAGE_SERVICES_TITLE = "Consultoria Cloud & DevOps";
+export const PAGE_SERVICES_DESCRIPTION =
+  "Pronto para acelerar a inovação, otimizar fluxos de trabalho e preparar sua infraestrutura para o futuro?";
+/*
+ * Tags - Page titles and descriptions
+ */
+export const PAGE_TAGS_TITLE = "Explore as tags";
+export const PAGE_TAGS_DESCRIPTION =
+  "Navegue pelas tags e busque os artigos relacionados a cada tag";
