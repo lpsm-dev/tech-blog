defd5601f10f1e42c3a6c273e3c58ba1fe80f31b
lpsm-dev-github <lpsm-dev@protonmail.com>
2025-02-01 08:58:57 -0300

    fix: setup search with blog and tags

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,7 +1,7 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 import { getUrl } from "../utils/url";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
   const posts = await getCollection("blog", ({ data }) => {
